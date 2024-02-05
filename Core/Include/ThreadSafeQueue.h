e7344a85a63f1484e3c3a454a86139e7c8e6a258
ArianHeight <edmondxue@outlook.com>
2024-02-05 00:04:09 -0800

    Spelling error fixed.

--- Core/Include/ThreadSafeQueue.h
+++ Core/Include/ThreadSafeQueue.h
@@ -10,7 +10,7 @@ namespace core
 	{
 	private:
 		std::mutex m;
-		std::queue data;
+		std::queue<T> data;
 
 	public:
 		ThreadSafeQueue() {}
@@ -18,43 +18,43 @@ namespace core
 
 		void readData(std::function<void(const std::vector<T>&)> readFunc) const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			readFunc(data);
 		}
 
 		const size_t size() const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			return data.size();
 		}
 
 		const bool empty() const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			return data.size();
 		}
 
 		void modifyData(std::function<void(std::queue<T>)&> modifyFunc)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			modifyFunc(data);
 		}
 
 		void push(const T& item)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.push(item);
 		}
 
 		void push(T&& item)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.push(item);
 		}
 
 		void pop()
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.pop();
 		}
 	};
--- Core/Include/ThreadSafeVector.h
+++ Core/Include/ThreadSafeVector.h
@@ -18,55 +18,55 @@ namespace core
 
 		void readData(std::function<void(const std::vector<T>&)> readFunc) const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			readFunc(data);
 		}
 
 		const size_t capacity() const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			return data.capacity();
 		}
 
 		const size_t size() const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			return data.size();
 		}
 
 		const bool empty() const
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			return data.empty();
 		}
 
 		void modifyData(std::function<void(std::vector<T>)&> modifyFunc)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			modifyFunc(data);
 		}
 
 		void reserve(size_t n)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.reserve(n);
 		}
 
 		void push_back(const T& newItem)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.push_back(newItem);
 		}
 
 		void push_back(T&& newItem)
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.push_back(newItem);
 		}
 
 		void pop_back()
 		{
-			std::unique_lock<std::mutext> scopedLock(m);
+			std::unique_lock<std::mutex> scopedLock(m);
 			data.pop_back();
 		}
 	};
